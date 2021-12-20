053d0d339c50ed5da5b6d49d71e1109f7b8d17ba
daianaryan <88803959+daianaryan@users.noreply.github.com>
2021-12-20 19:58:44 -0300

    Create nosotros.php

---
+++ nosotros.php
@@ -0,0 +1,63 @@
+<?php include 'header.php'; ?>
+				
+<section data-stellar-background-ratio="0.5">
+  <div id="home" class="position-relative text-center"></div>
+  
+  <div class="position-absolute top-50 start-50 translate-middle h-50">			
+    <h5 class="text-danger fw-light">NOSOTROS</h5>					
+  </div> 
+</section>
+
+<div class="container-fluid m-2">
+		<ul class="breadcrumb mb-0 icono-prop">
+			<li class="breadcrumb-item">
+				<a href="index.php"> 
+					<i class="fas fa-home text-danger"></i>
+				</a>
+			</li>
+			<li class="breadcrumb-item">
+				<span>Nosotros</span>
+			</li>
+		</ul>
+	</div>
+
+    <section>                
+        <div class="d-flex justify-content-center mb-4">
+            <div class="d-block" style="width: 48rem;">
+                <div class="card-body text-center">
+                    <i class="fab fa-accusoft"></i>
+                  <h5 class="card-title" style="font-size: 24px"><b><small>Nosotros</small></b></h5>                  
+                  <p class="card-text" style="font-size: 16px"><small>Contamos con más de 30 años de experiencia en asesoría inmobiliaria, consultoría y gestoría. A lo largo de todos estos años, acompañamos a nuestros clientes mediante la realización de más de 600.000 tareas de gestión guiados por los más altos estándares de calidad y seguridad.</small> </p>
+                </div>                               
+                <div class="d-flex">
+                    <div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+                        <div class="card-body text-center shadow-lg" >
+                            <i class="fas fa-warehouse"></i>
+                            <h5 class="card-title" style="font-size: 14px"><b>Misión</b></h5>                  
+                            <p class="card-text" style="font-size: 14px"><small>Ser reconocidos por nuestro profesionalismo, transparencia y seguridad. Nuestra meta es continuar siendo una empresa de servicios 360º, generando estrategias integrales para responder a las más complejas demandas de un mercado en constante diversificación..</small></p>
+                        </div>
+                      </div>
+                      <div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+                        <div class="card-body text-center shadow-lg">
+                            <i class="fas fa-glasses"></i>
+                          <h5 class="card-title" style="font-size: 14px"><b>Visión</b></h5>                  
+                          <p class="card-text" style="font-size: 14px"><small>Proveer soluciones a nuestros clientes, brindando seguridad, respaldo, confiabilidad y eficacia a través de distintas metodologías buscando la mejora constante.</small></p>
+                        </div>
+                      </div>
+                      <div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+                        <div class="card-body text-center shadow-lg">
+                            <i class="far fa-handshake"></i>
+                          <h5 class="card-title" style="font-size: 14px"><b>Valores</b></h5>                  
+                          <p class="card-text" style="font-size: 14px"><small>Somos un equipo de profesionales que trabaja diariamente con compromiso, celeridad y transparencia.</small></p>
+                        </div>
+                      </div>
+                </div>
+              </div>              
+        </div>      
+    </section>
+
+	<?php include 'footer.php'; ?>
+    <?php include 'script.php'; ?>
+
+</body>
+</html>
