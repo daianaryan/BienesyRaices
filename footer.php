89dc2b3cd77165ee784361840bd9f95a509d59f1
daianaryan <88803959+daianaryan@users.noreply.github.com>
2021-12-20 19:57:49 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,59 @@
+
+<!-- Pie de Pagina-->	
+	<footer class="container-fluid bg-dark py-4">		
+		<div class="wrapper__footer bg-theme-footer text-white">
+			<div class="container">
+				<!-- Nav Superior -->
+				<div class="row">		
+					<div class="d-flex">
+						<!-- Datos y Link -->	
+						<div class="container widget__footer d-flex">
+							
+							<!-- Nav Izquierdo -->
+							<a href="index.php" class="logo"><img src="img/Logo.jpg" width="75" height="75" alt="" class="footet-contact logo-footer justify-content-center img-transparent"></a>
+							<div class="container-fluid col-md-6  ">								
+								<ul class="footet-contact list-unstyled mb-0 mt-3 me-4 p-4 justify-content-center">
+									<li><b><i class="fa fa-map-marker"></i></b><span> Belgrano 1, Bernal</span></li>
+									<li><b><i class="fa fa-phone"></i></b><span> +54 (011) 1234-5678</span></li>
+									<li><a href="https://api.whatsapp.com/send?phone=+541112345678&amp;text=Hola%20Alfonso,%20los%20vi%20desde%20la%20web%20y%20queria%20consultar%20por%20" class="text-decoration-none text-white" target="_blank">
+										<i class="fa fa-whatsapp"></i><b></b><span> +54 (011) 1234-5678</span></a>
+									</li>
+									<li><a href="mailto:mail@gmail.com" class="text-decoration-none text-white" target="_blank">
+										<b><i class="fa fa-envelope"></i></b><span> mail@gmail.com</span></a>
+									</li>
+								</ul>								
+							</div>							
+							<!-- Nav Derecho -->
+							<div class="container-fluid col-md-6">
+								<ul class="d-flex p-2 m-2 footer-link justify-content-center">
+									<a class="nav-link active" aria-current="page" href="index.php"><small>Inicio</small></a>									
+									<a class="nav-link active" aria-current="page" href="tasaciones.php"><small>Tasaciones</small></a>
+									<a class="nav-link active" aria-current="page" href="servicios.php"><small>Servicios</small></a>
+									<a class="nav-link active" aria-current="page" href="nosotros.php"><small>Quienes Somos</small></a>
+									<a class="nav-link active" aria-current="page" href="contacto.php"><small>Contacto</small></a>
+								</ul>
+							</div> 							
+						</div> 																				
+					</div> <!-- END Contacto -->		
+					<br>
+					<hr> 	
+				</div>
+
+				<!-- Nav Inferior -->
+				<div class="container-fluid">
+					<!-- Redes Sociales -->
+					<div class="row">
+						<div class="widget__footer">
+							<h5 class="d-flex justify-content-center text-danger"><b>Nuestras redes</b></h5>
+							<div class="d-flex justify-content-center w-100 col-8">
+								<div class="d-flex">									
+									<a class="nav-link mr-3 py-0 px-2 text-white" rel="noreferrer" href="https://www.facebook.com/ryancebeiro" target="_blank" aria-label="Facebook"><i class="fab fa-facebook"></i></a>									
+									<a class="nav-link mr-3 py-0 px-2 text-end justify-content-end text-white" rel="noreferrer" href="https://www.instagram.com/ryancebeiro" target="_blank" aria-label="Instagram"><i class="fab fa-instagram"></i></a>									
+								</div>				
+							</div>																		
+						</div>	
+					</div>	<!-- END Redes Sociales -->	
+				</div>
+			</div>
+		</div>
+	</footer> <!-- End Footer  -->
