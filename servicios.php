86f978565f2ed479a8d61fb8704b36ab31556d96
daianaryan <88803959+daianaryan@users.noreply.github.com>
2021-12-20 19:59:26 -0300

    Create servicios.php

---
+++ servicios.php
@@ -0,0 +1,111 @@
+<?php include 'header.php'; ?>
+
+<section data-stellar-background-ratio="0.5">
+  <div id="home" class="position-relative text-center"></div>
+  
+  <div class="position-absolute top-50 start-50 translate-middle h-50">			
+    <h5 class="text-danger fw-light">SERVICIOS</h5>					
+  </div> 
+</section>
+	
+	<div class="container-fluid m-2">
+		<ul class="breadcrumb mb-0 icono-prop">
+			<li class="breadcrumb-item">
+				<a href="index.php"> 
+					<i class="fas fa-home text-danger"></i>
+				</a>
+			</li>
+			<li class="breadcrumb-item">
+				<span><small>Servicios</small></span>
+			</li>
+		</ul>
+	</div>
+
+
+    <section>  	              
+        <div class="d-flex justify-content-center mb-4">
+            <div class="d-block" style="width: 48rem;">                                              
+                <div class="card-body text-center">
+				<i class="fab fa-accusoft"></i>
+                  <h5 class="card-title" style="font-size: 24px"><b><small>Servicios</small></b></h5>                  
+                  <p class="card-text" style="font-size: 16px"><small>Contamos con más de 30 años de experiencia en asesoría inmobiliaria, consultoría y gestoría. A lo largo de todos estos años, acompañamos a nuestros clientes mediante la realización de más de 600.000 tareas de gestión guiados por los más altos estándares de calidad y seguridad.</small> </p>
+                </div>  
+					<div class="d-block">
+						<!--Divisor 1-->
+						<div class="d-flex">
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="fas fa-warehouse"></i>
+									<h5 class="card-title" style="font-size: 14px"><b><small>Gestión</small></b></h5>                  
+									<p class="card-text" style="font-size: 14px"><small>Ser reconocidos por nuestro profesionalismo, transparencia y seguridad. Nuestra meta es continuar siendo una empresa de servicios 360º, generando estrategias integrales para responder a las más complejas demandas de un mercado en constante diversificación..</small></p>
+								</div>
+							</div>
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="far fa-handshake"></i>
+								  <h5 class="card-title" style="font-size: 14px"><b><small>Asesoramiento</small></b></h5>                  
+								  <p class="card-text" style="font-size: 14px"><small>Proveer soluciones a nuestros clientes, brindando seguridad, respaldo, confiabilidad y eficacia a través de distintas metodologías buscando la mejora constante.</small></p>
+								</div>
+							</div>
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="fas fa-ad"></i>
+								  <h5 class="card-title" style="font-size: 14px"><b><small>Publicidad</small></b></h5>                  
+								  <p class="card-text" style="font-size: 14px"><small>Somos un equipo de profesionales que trabaja diariamente con compromiso, celeridad y transparencia.</small></p>
+								</div>
+							</div>
+						</div>
+
+						<!--Divisor 2-->
+						<div class="d-flex">
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="fas fa-pencil-alt"></i>
+									<h5 class="card-title" style="font-size: 14px"><b><small>Contratos</small></b></h5>                  
+									<p class="card-text" style="font-size: 14px"><small>Ser reconocidos por nuestro profesionalismo, transparencia y seguridad. Nuestra meta es continuar siendo una empresa de servicios 360º, generando estrategias integrales para responder a las más complejas demandas de un mercado en constante diversificación..</small></p>
+								</div>
+							</div>
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="fas fa-hand-holding-usd"></i>
+								  <h5 class="card-title" style="font-size: 14px"><b><small>Cobranza</small></b></h5>                  
+								  <p class="card-text" style="font-size: 14px"><small>Proveer soluciones a nuestros clientes, brindando seguridad, respaldo, confiabilidad y eficacia a través de distintas metodologías buscando la mejora constante.</small></p>
+								</div>
+							</div>
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="fas fa-chart-line"></i>
+								  <h5 class="card-title" style="font-size: 14px"><b><small>Administración</small></b></h5>                  
+								  <p class="card-text" style="font-size: 14px"><small>Somos un equipo de profesionales que trabaja diariamente con compromiso, celeridad y transparencia.</small></p>
+								</div>
+							</div>
+						</div>					  
+
+						<!--Divisor 3-->
+						<div class="d-flex justify-content-center">
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="fas fa-file-signature"></i>
+								  <h5 class="card-title" style="font-size: 14px"><b><small>Compra-Venta</small></b></h5>                  
+								  <p class="card-text" style="font-size: 14px"><small>Proveer soluciones a nuestros clientes, brindando seguridad, respaldo, confiabilidad y eficacia a través de distintas metodologías buscando la mejora constante.</small></p>
+								</div>
+							</div>
+							<div class="card m-2 me-4 shadow-lg" style="width: 18rem;">
+								<div class="card-body text-center shadow-lg">
+									<i class="fas fa-dollar-sign"></i>
+								  <h5 class="card-title" style="font-size: 14px"><b><small>Tasación</small></b></h5>                  
+								  <p class="card-text" style="font-size: 14px"><small>Somos un equipo de profesionales que trabaja diariamente con compromiso, celeridad y transparencia.</small></p>
+								</div>
+							</div>
+						</div>					
+					</div>
+				
+              </div>              
+        </div>      
+    </section>
+
+	<?php include 'footer.php'; ?>
+	<?php include 'script.php'; ?>
+
+</body>
+</html>
